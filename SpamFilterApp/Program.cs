bb5d48bedb342239ae441ce3991473679fb055fb
артур панин <ar-panin.panin2011@yandex.ru>
2021-05-24 18:19:56 +0300

    Добавьте файлы проекта.

---
+++ SpamFilterApp/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using SpamFilterLibrary;
+using System.Net;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+
+
+namespace SpamFilterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpamFilter oSF = new SpamFilter();
+            string szLine;
+
+            TextReader oLearnFile = File.OpenText(@"Data\LearnData.txt");
+            while ((szLine = oLearnFile.ReadLine()) != null)
+            {
+                oSF.Learn(szLine.Substring(szLine.IndexOf(',') + 1), szLine.ToLower().StartsWith("spam,") ? MessageType.Spam : MessageType.Ham);
+            }
+            oLearnFile.Close();
+            oSF.Transparent();
+
+            ///подсчёт числа слов
+            var rowsInLine = 60;
+            var dict = new Dictionary<string, List<int>>();
+            string[] line; //объявляем массив слов
+            int countOfRows = 0;
+            using (var sr = new StreamReader(@"Data\TestData.txt")) //создаем стримридер
+                while (!sr.EndOfStream) //пока наш экземпляр ридера не достиг конца потока
+                {
+                    line = sr.ReadLine().ToLower().Split(" < > <? >? +:=1234567890 ''%-<><p></p>,.?!\'()\"".ToCharArray(), StringSplitOptions.RemoveEmptyEntries); //считываем строку из потока, переводим в нижний регистр, разбиваем на отдельные слова по массиву разделителей и убираем пустые вхождения
+                    countOfRows++;
+                    foreach (var word in line) //перебираем весь массив слов
+                    {
+                        if (!dict.ContainsKey(word)) dict.Add(word, new List<int>() { countOfRows / rowsInLine + 1 }); //если в словаре нет такого ключа, добавляем в словарь новую пару, ключ слово, значение новый список из одного эл-та - номера страницы...хотя, думаю, лучше все же номера строк заносить, потом можно будет переделать вывод на строки,абзацы,страницы,тома и т.д.:)
+                        else dict[word].Add(countOfRows / rowsInLine + 1); //если же значение есть, то добавляем в список еще одно значение
+                    }
+                }
+            var result = dict.OrderBy(x => x.Key).GroupBy(x => x.Key[0]).OrderBy(x => x.Key); //словарь сортируем по ключу(по словам в алфавитном порядке), группируем по первой букве(алфавитный указатель), еще раз сортируем(это излишне, когда писал, перестарался, лучше убрать, не мешает, но и лишняя работа ни к ченму)
+
+            foreach (var group in result) //перебираем последовательность
+            {
+                Console.WriteLine(group.Key.ToString().ToUpper()); //выводим ключ группы, в верхнем регистре
+                foreach (var item in group) //перебираем уже саму группу
+                {
+                    Console.WriteLine(item.Key.PadRight(20, '.') + " " + item.Value.Count + ":" + string.Join(" ", item.Value.Distinct())); //выводим ключ группы с выравниванием + количество записей в списке List + само содерживое списка через пробел, с удалением дублей
+                }
+            }
+
+            TextReader oTestFile = File.OpenText(@"Data\TestData.txt");
+            while ((szLine = oTestFile.ReadLine()) != null)
+            {
+                MessageType eExpectedType = szLine.ToLower().StartsWith("spam,") ? MessageType.Spam : MessageType.Ham;
+                MessageType eType = oSF.Classify(szLine.Substring(szLine.IndexOf(',') + 1));
+
+                if (eExpectedType != eType)
+                {
+                    Console.Write("!Спам - {0}{1}", szLine.Substring(0, 35) + "...", Environment.NewLine);
+                }
+                else
+                {
+                    Console.Write("Спам - \t{0}{1}", szLine.Substring(0, 35) + "...", Environment.NewLine);
+                }
+            }
+            oTestFile.Close();
+
+            Console.WriteLine("Вывести все спам-слова? Y or N");
+            string TextRead = Console.ReadLine();
+
+            if (TextRead == "да" || TextRead == "Да" || TextRead == "lf")
+            {
+                string[] lines = File.ReadAllLines(@"Data\1.txt");
+                foreach (var line1 in lines)
+                {
+                    Console.WriteLine(line1);
+                }
+            }
+
+            else 
+            {
+                Console.WriteLine("Ну ладно");
+            }
+
+            if (TextRead == "да" || TextRead == "Да" || TextRead == "lf")
+            {
+                //цикл перебора слов из файла
+                StreamReader f = new StreamReader(@"Data\2.txt");
+                while (!f.EndOfStream)
+                {
+                    string s = f.ReadLine();
+
+                    ///Работаем с rusvectors
+                    Console.WriteLine("Найдём синонимы?");
+                    string TextRead1 = Console.ReadLine();
+
+                    if (TextRead1 == "нет" || TextRead == "Нет" || TextRead == "ytn")
+                    {
+                        Console.WriteLine("Ну ладно");
+                        return;
+                    }
+
+                    string apiURL1 = "https://rusvectores.org/";
+                    string word1 = s;
+                    string model1 = "ruwikiruscorpora_upos_skipgram_300_2_2019";
+                    string format1 = "json";
+                    WebClient client1 = new WebClient();
+                    string downloadString1 = client1.DownloadString(apiURL1 + "/" + model1 + "/" + word1 + "/api/" + format1 + "/");
+
+                    Console.WriteLine("Запрашиваем с - " + apiURL1 + "/" + model1 + "/" + word1 + "/api/json/");
+                    Console.WriteLine("Ищем синонимы слова - " + word1 + "!" + '\n' + "Слова получаются такие:");
+                    //Console.WriteLine("Слова получается у нас такие - " + downloadString1);
+
+                    List<String> words = new List<String>();
+                    Regex rx = new Regex(@"(\w+)_(\w+)(.?: \d+)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                    MatchCollection matches = rx.Matches(downloadString1);
+                    
+                    foreach (Match match in matches)
+                        words.Add(match.Groups[1].Value);
+
+                    foreach (String w in words)
+
+                    if (words.Count == 0)
+                    {
+                        Console.WriteLine("Синонимов нет");
+                    }
+                    else
+                    { 
+                        Console.WriteLine("{0}", w);
+                    } 
+                }
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ SpamFilterLibrary/SpamFilter.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpamFilterLibrary
+{
+	public enum MessageType
+	{
+		Spam,Ham
+	}
+
+	public class SpamFilter
+	{
+		private static int _Max = 1000;
+		private static decimal _Significance = 0.5m;
+
+		private int _Trigger = 0;
+
+		private class BaseFact
+		{
+			public decimal Spam;
+			public decimal Ham;
+
+			public bool IsTransparent
+			{
+				get => ( Math.Abs( ( Spam - Ham ) ) / ( Spam + Ham ) ) <= SpamFilter._Significance;
+			}
+		}
+
+		public List<string> _TransparentList = new List<string>() 
+		{ "купон", "промокод", "скидка", "акция","подборка", "новинка", "кэшбэк", "интернет-магазин", "копить", "путешествие", 
+			"новинка", "ссылка", "бренд", "выгодно", "бесплатно", "история", "праздник", "хиты", "Знакомьтесь", "обменивай", "подарок", "узнай больше", "подробнее", "образ", "идея", "дарим", 
+			"путешествие", "возвращаем", "друг", "клиент", "распродажа", "код", "оформите", "лото", "джекпот", "победитель", "сэкономьте", "бюджет", "покупатель", "выгодно", "подпишитесь", 
+			"специальная цена", "на сайте", "Оформите кредит", "кредит", "приз", "аренда", "продажа", "ограничено", "лучшие товары", "Узнайте в новинках", "Избранные товары", "мир скидок",
+			"Успейте купить", "в наличии", "вналичии", "ПЕРВЫЙ ЗАКАЗ", "Спешите купить", "СУПЕРАКЦИИ", "бонус", "Акция дня", "выгода", "кешбэк", "экономь", "штраф", "выгодные цены",
+			"последний день", "Дни выгоды", "Подробнее по сссылке", "курорт", "мультимиллионер", "Спортлото", "приглашаем", "отзыв"};
+		
+		private Dictionary<string, BaseFact> _BaseFacts = new Dictionary<string, BaseFact>( );
+
+		private string[ ] Message2Words ( string Message )
+		{
+			string szMessage = Message.ToLower( );
+			List<char> oPunctuations = szMessage.Where( Char.IsPunctuation ).Distinct( ).ToList( );
+
+			oPunctuations.AddRange( new char[ ] { ' ', '<', '>' } );
+
+			string[ ] oWords = szMessage.Split( oPunctuations.ToArray( ) ).Where( szWord => szWord.Length > 2 ).ToArray( );
+
+			return oWords.Except( _TransparentList ).ToArray( );
+		}
+
+		public void Transparent ( )
+		{
+			_TransparentList.AddRange( _BaseFacts.Where( oItem => oItem.Value.IsTransparent ).ToDictionary( oItem => oItem.Key, oItem => oItem.Value ).Keys.ToList( ) );
+			_BaseFacts = _BaseFacts.Where( oItem => !oItem.Value.IsTransparent ).ToDictionary( oItem => oItem.Key, oItem => oItem.Value );
+		}
+
+		public void Learn ( string Message, MessageType Type, string[ ] List = null )
+		{
+			string[ ] szList = List ?? Message2Words( Message );
+			decimal nTemp;
+
+			foreach ( string szWord in szList )
+			{
+				BaseFact oValue = _BaseFacts.ContainsKey( szWord ) ? _BaseFacts[szWord] : new BaseFact( );
+
+				nTemp = (Type == MessageType.Spam) ? oValue.Spam++ : oValue.Ham++;
+
+				_BaseFacts[szWord] = oValue;
+			}
+		}
+
+		public MessageType Classify ( string Message )
+		{
+			string[ ] szList = Message2Words( Message );
+			Dictionary<string, BaseFact> oList = _BaseFacts.Where( oFact => szList.Contains( oFact.Key ) ).ToDictionary( oItem => oItem.Key, oItem => oItem.Value );
+			MessageType eType = oList.Sum( oItem => oItem.Value.Spam ) > oList.Sum( oItem => oItem.Value.Ham ) ? MessageType.Spam : MessageType.Ham;
+
+			Learn( Message, eType, szList );
+
+			if ( ++_Trigger > _Max )
+			{
+				_Trigger = 0;
+				Transparent( );
+			}
+			return ( eType );
+		}
+	}
+}
